975c0a96b8d1e75a93614d67b4d63300341c9a28
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-04-14 11:23:30 +0900

    성적처리 프로그램 v3 - 성적 조회 및 상세조회 loadSungJuk

--- src/main/java/qnfzks3/project/sungjuk/dao/SungJukV3DAOImpl.java
+++ src/main/java/qnfzks3/project/sungjuk/dao/SungJukV3DAOImpl.java
@@ -6,42 +6,72 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class SungJukV3DAOImpl implements SungJukV3DAO {
-    private Scanner sc = null;
-    private List<SungJukVO> sjs = null;
-    private int idx = 0;
-
     private String fname = "c:/Java/sungjukv3.dat";
-    private FileWriter fw=null;  // 처음에는 항상 초기화 null을 써주자
-    private FileReader fr=null;
-    private BufferedWriter bw=null;
-    private BufferedReader br=null;
+    private FileWriter fw = null;
+    private FileReader fr = null;
+    private BufferedWriter bw = null;
+    private BufferedReader br = null;
 
     @Override
     public boolean saveSungJuk(SungJukVO sj) {
-
-        //생성된 성적 데이터를 파일로 저장
+        // 생성된 성적 데이터를 파일에 저장
         try {
-            fw=new FileWriter(fname,true);
-            bw=new BufferedWriter(fw);
-            bw.write(sj.toString()); //파일에기록
+            // 파일기록시 추가append 기능 활성화
+            fw = new FileWriter(fname, true);
+            bw = new BufferedWriter(fw);
+
+            bw.write(sj.toString());  // 파일에 기록
         } catch (Exception ex) {
-            System.out.println("성적 데이터 저장중 오류발생!");
+            System.out.println("성적 데이터 저장중 오류발생!!");
             System.out.println(ex.getMessage());
             return false;
         } finally {
-            if (bw != null) {try {bw.close();} catch (Exception ex) {}}
-            if (bw != null) {try {fw.close();} catch (Exception ex) {} //쓰기만 적ㅇㅁ
-            }
+            if (bw != null) try { bw.close(); } catch (Exception ex) { }
+            if (fw != null) try { fw.close(); } catch (Exception ex) { }
         }
+
         return true;
     }
 
+    // 파일에 저장된 성적 데이터들을 모두 읽어서
+    // ArrayList 객체에 저장하고 리턴
     @Override
     public List<SungJukVO> loadSungJuk() {
-        return null;
+        List<SungJukVO> sjdata = new ArrayList<>();
+
+        try {
+            fr = new FileReader(fname);
+            br = new BufferedReader(fr);
+
+            while(br.ready()) {
+                // 파일에서 성적데이터 한줄을 읽어서
+                String[] val = br.readLine().split("[ ]");
+
+                // SungJuk 객체에 개별 값을 저장하고
+                SungJukVO sj = new SungJukVO(
+                        val[0], Integer.parseInt(val[1]),
+                        Integer.parseInt(val[2]), Integer.parseInt(val[3]) );
+                sj.setTot( Integer.parseInt(val[4]) );
+                sj.setAvg( Double.parseDouble(val[5]) );
+                sj.setGrd( val[6].charAt(0) );
+
+                // ArrayList 객체에 저장
+                sjdata.add(sj);
+            }
+        } catch (Exception ex) {
+            System.out.println("성적 데이터 저장중 오류발생!!");
+            System.out.println(ex.getMessage());
+        } finally {
+            if (br != null) try { br.close(); } catch (Exception ex) { }
+            if (fr != null) try { fr.close(); } catch (Exception ex) { }
+        }
+
+        return sjdata;
     }
+
 }
