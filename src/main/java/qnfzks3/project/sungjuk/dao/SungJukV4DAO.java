5f8b5294b826994d5d40f0fbcdaaa4d00cc2358d
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-04-18 14:33:15 +0900

    성적처리 프로그램 v4 mariaDB select where , delete 성적 상세조회 시 학생번호 사용
    SungJukVO toString 수정

--- src/main/java/qnfzks3/project/sungjuk/dao/SungJukV4DAO.java
+++ src/main/java/qnfzks3/project/sungjuk/dao/SungJukV4DAO.java
@@ -10,7 +10,8 @@ public interface SungJukV4DAO {  //파일을 기록하는 부분만 적었다.  
     SungJukVO selectOneSungJuk(int sjno);
     int updateSungJuk(SungJukVO sj);
     int deleteSungJuk(int sjno);
-
+// 인터페이스로 미리 클래스에 어떤 함수를 사용했는지 한눈에 알 수 있다. - 목차같은 느낌이라고 생각해도됨
+// 이러한 인터페이스는 그때 그때 클래스안에 함수를 정의해서 사용하지 않고 사용할 수 있다. 사용할 땐 오버라이딩(부모클래스의 함수 내용을 수정한다는 의미)을 사용해 사용
 
 
 
